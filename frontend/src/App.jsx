2e35b32fcf0ee02eefcd4d9ad097ba546fa82eee
dionjoshualobo <23h13.joshua@sjec.ac.in>
2024-12-27 22:10:46 +0530

    Added empty component files for navigation options. Added daisyui. Added Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx' 
+import SignUpPage from './pages/SignUpPage.jsx'
+import SettingsPage from './pages/SettingsPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
 
 const App = () => {
   return (
-    <div className="text-red-800">Hello</div>
+    <div>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+
+      </Routes>
+
+
+
+
+    </div>
   )
 }
 
