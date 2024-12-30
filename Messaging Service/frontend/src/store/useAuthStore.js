828a80012715af10e9f163b7ae06d7414966ec5d
dionjoshualobo <23h13.joshua@sjec.ac.in>
2024-12-30 20:41:09 +0530

    frontend nearly done

--- Messaging Service/frontend/src/store/useAuthStore.js
+++ Messaging Service/frontend/src/store/useAuthStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
-
+import { io } from "socket.io-client";
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
@@ -17,6 +17,7 @@ export const useAuthStore = create((set, get) => ({
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
+
       set({ authUser: res.data });
       get().connectSocket();
     } catch (error) {
